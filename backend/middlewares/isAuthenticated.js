c4fe685a49b86079406e39f759c5377adfdbf220
Susan Zhang <67386805+susanzzz123@users.noreply.github.com>
2022-08-02 21:12:35 -0500

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,3 +1,4 @@
+//checks if a user attemped to do some kind of request is logged in
 const isAuthenticated = (req, res, next) => {
   const user = req.session.username
   if (user === '' || user === undefined) {
